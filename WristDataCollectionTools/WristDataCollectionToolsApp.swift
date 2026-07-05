d02d09a1dc080e46ae5d6361b3650c8b76fcf792
fikrahdamars <fikrahdamar7@gmail.com>
2026-07-05 13:38:31 +0700

    initial commit

---
+++ WristDataCollectionTools/WristDataCollectionToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristDataCollectionToolsApp.swift
+//  WristDataCollectionTools
+//
+//  Created by Fikrah Damar Huda on 05/07/26.
+//
+
+import SwiftUI
+
+@main
+struct WristDataCollectionToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WristDataCollectionToolsWatch Watch App/WristDataCollectionToolsWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristDataCollectionToolsWatchApp.swift
+//  WristDataCollectionToolsWatch Watch App
+//
+//  Created by Fikrah Damar Huda on 05/07/26.
+//
+
+import SwiftUI
+
+@main
+struct WristDataCollectionToolsWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
